5b7850ce28e9916af037837785cb09c95472130e
irfan6321 <irfandhbri220805@gmail.com>
2025-09-30 19:29:29 +0530

    new commit

--- carbon_backend/frontend/frontend.py
+++ carbon_backend/frontend/frontend.py
@@ -5,7 +5,7 @@ import os
 import time
 
 # --- Configuration ---
-BACKEND_URL = "https://a1b2-c3d4-e5f6-g7h8.ngrok-free.app" # PASTE YOUR NGROK URL HERE
+BACKEND_URL = "https://egotistic-elouise-nonmythical.ngrok-free.dev" # PASTE YOUR NGROK URL HERE
 
 st.set_page_config(layout="wide")
 st.title("Drone Imagery Carbon Sequestration Platform")
